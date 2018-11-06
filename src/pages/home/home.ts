2c24e0336b42b639dab34a04c0372d15d2c058ba
Antoine Blanchard <antoine.blanchard@epitech.eu>
2018-11-07 00:02:36 +0800

    end

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,106 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import {NavController, NavParams} from 'ionic-angular';
+import {TrackPage} from '../track/track';
 
 @Component({
-  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tracks: string[];
+  sSearchTrack = '';
 
+  constructor(public http: HttpClient, public navCtrl: NavController, public navParams: NavParams) {
+    this.sSearchTrack = navParams.get('search');
+  }
+
+  obtainSpotifyAccessToken() {
+    return new Promise((resolve, reject) => {
+      let params = new URLSearchParams();
+      params.append('grant_type', 'client_credentials');
+      let options = {
+        headers: new HttpHeaders({
+          'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+          'Authorization': 'Basic ' + btoa("c0029216bd4544f2b40c6d5953a6707e:b2edd33af8594ed194718b41e92a29fd")
+        }),
+      };
+
+      this.http.post('spotify-account/api/token', params.toString(), options)
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Invalid Credentials');
+          });
+    });
+  }
+
+  async spotifySearch(sSearch) {
+    let oToken: any = await this.obtainSpotifyAccessToken();
+    return new Promise((resolve, reject) => {
+      let options = {
+        headers: new HttpHeaders({
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + oToken.access_token,
+        }),
+      };
+
+      this.http.get('https://api.spotify.com/v1/search?q=' + sSearch + '&type=track', options)
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Error Search');
+          });
+    });
+  }
+
+  async GlobalSearch(sSearch) {
+    let tmpTracks = [];
+    let tmpSearch = this.sSearchTrack;
+
+    return new Promise((resolve, reject) => {
+      this.spotifySearch(sSearch.trim()).then((data: any) => {
+          if (data && data.tracks.total > 0) {
+            let items = data.tracks.items;
+            items.forEach(function (value) {
+              tmpTracks.push({'search': tmpSearch, 'name': value.name, 'artist': value.artists['0'].name, 'type': "spotify", 'image': value.album.images['0'].url, 'url': value.external_urls.spotify});
+            });
+          }
+        }
+      ).catch(function (e) {});
+      resolve(tmpTracks);
+    });
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.tracks = [];
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+    this.sSearchTrack = val;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.GlobalSearch(val.trim()).then((data: any) => {
+          return (this.tracks = data);
+        }
+      );
+    }
+  }
+
+  goTOTrackPage(track) {
+    let dataTrack = track;
+
+    this.navCtrl.push(TrackPage, {
+      track: dataTrack
+    });
   }
 
 }
---
+++ src/pages/track/track.ts
@@ -0,0 +1,167 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import {NavController, NavParams} from "ionic-angular";
+import {xml2json} from "xml-js";
+import {HomePage} from '../home/home';
+
+@Component({
+  templateUrl: 'track.html'
+})
+export class TrackPage {
+
+  spotifyTrack = [];
+
+  constructor(public http: HttpClient, public navParams: NavParams , public navCtrl: NavController) {
+    this.spotifyTrack = navParams.get('track');
+    this.GlobalSearch().then((data: any) => {
+      this.spotifyTrack['externes_url'] = data;
+    });
+  }
+
+  obtainSpotifyAccessToken() {
+    return new Promise((resolve, reject) => {
+      let params = new URLSearchParams();
+      params.append('grant_type', 'client_credentials');
+      let options = {
+        headers: new HttpHeaders({
+          'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+          'Authorization': 'Basic ' + btoa("c0029216bd4544f2b40c6d5953a6707e:b2edd33af8594ed194718b41e92a29fd")
+        }),
+      };
+
+      this.http.post('spotify-account/api/token', params.toString(), options)
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Invalid Credentials');
+          });
+    });
+  }
+
+  async spotifySearch(sSearch) {
+    let oToken: any = await this.obtainSpotifyAccessToken();
+    return new Promise((resolve, reject) => {
+      let options = {
+        headers: new HttpHeaders({
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + oToken.access_token,
+        }),
+      };
+
+      this.http.get('https://api.spotify.com/v1/search?q=' + sSearch + '&type=track', options)
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Error Search');
+          });
+    });
+  }
+
+  async ItunesSearch() {;
+    return new Promise((resolve, reject) => {
+      this.http.get('https://itunes.apple.com/search?term=' + this.spotifyTrack['name'] + '+' + this.spotifyTrack['artist'])
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Error Search');
+          });
+    });
+  }
+
+  async YoutubeSearch() {;
+
+    let youtubeToken = 'AIzaSyCvEZh-oz8CBw06u_-LNsujRRJMzbOK0iM'
+
+    return new Promise((resolve, reject) => {
+      this.http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + this.spotifyTrack['name'] + '+' + this.spotifyTrack['artist'] + '&type=video&key=' + youtubeToken)
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject('Error Search');
+          });
+    });
+  }
+
+  async DeezerSearch() {;
+    return new Promise((resolve, reject) => {
+      let headers = new HttpHeaders({
+          'Accept': 'text',
+          'Content-Type': 'text',
+        });
+
+      this.http.get('deezer/search/?q=' + this.spotifyTrack['name'] + '+' + this.spotifyTrack['artist'] + '&output=xml', {headers: headers, responseType: 'text'})
+        .subscribe(
+          data => {
+            resolve(JSON.parse(xml2json(data.toString(), {compact: true, ignoreComment: true, spaces: 4})).root);
+          },
+          err => {
+            reject('Error Search');
+          });
+    });
+  }
+
+  async GlobalSearch() {
+    let tmpTracks = [{itunes: {}, youtube: {}, deezer: {}}];
+
+    return new Promise((resolve, reject) => {
+      this.ItunesSearch().then((data: any) => {
+          if (data && data.resultCount > 0) {
+            let items = data.results;
+            tmpTracks['0'].itunes = {'itunesUrl': items['0'].trackViewUrl};
+          }
+        }
+      ).catch(function (e) {});
+      this.YoutubeSearch().then((data: any) => {
+          if (data && data.items.length > 0) {
+            let items = data.items;
+            tmpTracks['0'].youtube = {'youtubeUrl': 'https://www.youtube.com/watch?v=' + items['0'].id.videoId };
+          }
+        }
+      ).catch(function (e) {});
+      this.DeezerSearch().then((data: any) => {
+          if (data && data.total._cdata != "0") {
+            let items = data.data.track;
+            tmpTracks['0'].deezer = {'deezerUrl': items.link._cdata};
+          }
+        }
+      ).catch(function (e) {});
+      resolve(tmpTracks);
+    });
+  }
+
+  goToIndex() {
+    console.log(this.spotifyTrack['search']);
+
+    this.navCtrl.push(HomePage, {
+      search: this.spotifyTrack['search']
+    });
+  }
+
+
+  spotifyUrl() {
+    window.open(this.spotifyTrack['url'],"_system");
+  }
+
+  itunesUrl() {
+    window.open(this.spotifyTrack['externes_url']['0'].itunes.itunesUrl,"_system") ;
+  }
+
+  youtubeUrl() {
+    window.open(this.spotifyTrack['externes_url']['0'].youtube.youtubeUrl,"_system") ;
+  }
+
+  deezerUrl() {
+    window.open(this.spotifyTrack['externes_url']['0'].deezer.deezerUrl,"_system");
+  }
+
+}
